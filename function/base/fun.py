ba69533993f69b9706597ca9e25c41eecfa44fec
purofle <3272912942@qq.com>
2020-06-13 19:28:10 +0800

    del....

--- function/base/fun.py
+++
@@ -1,9 +0,0 @@
-from mirai import Mirai, Group, Member, MessageChain, At, Plain
-import asyncio
-import requests
-
-async def yiyan(app: Mirai, group: Group, member: Member,message: MessageChain):
-    if message.toString().find("一言") == -1:
-        return False
-
-
